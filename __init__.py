d47b4ea1a5a0c369be64a06fb558d76ed55e16c6
Karl Veskus <karl.veskus@gmail.com>
2017-12-08 05:33:27 +0200

    test

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	return 'Yo, it's working!'
+if __name__ == "__main__":
+	app.run()
