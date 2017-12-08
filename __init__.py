adc33e1e7a9ade0b6f8090ad636bdcae8f115a09
Karl Veskus <karl.veskus@gmail.com>
2017-12-08 06:13:11 +0200

    test

--- __init__.py
+++ __init__.py
@@ -1,37 +1,9 @@
-from flask import Flask, render_template, jsonify, url_for, request, flash,\
-                  redirect, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from flask_github import GitHub
-
-import config
-from models import Base, Article, Topic, User
-from database_seed import base_query, database_seed
-
-from datetime import date
-import requests
-
-engine = create_engine(config.DATABASE_CONNECTION)
-Base.metadata.bind = engine
-DBSession = scoped_session(sessionmaker(bind=engine))
-db_session = DBSession()
-
+from flask import Flask
 app = Flask(__name__)
-app.config.from_object('config')
-
-github = GitHub(app)
-
-api_route = config.API_ROUTE
-
 
 @app.route('/')
 def hello_world():
   return 'Hello from Flask!'
 
-
-
-# Server
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+  app.run()
