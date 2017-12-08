5d6a5fceef5da410cca40e8a2ef9e41d94141f93
Karl Veskus <karl.veskus@gmail.com>
2017-12-08 05:16:12 +0200

    Refactor project for deployment

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SECRET_KEY = '?\xbf,\xb4\x8d\xa3"<\x9c\xb0@\x0f5\xab,w\xee\x8d$0\x13\x8b83'
-DATABASE_CONNECTION = 'sqlite:///catalog.db'
+DATABASE_CONNECTION = 'postgresql://catalog:password@localhost/catalog'
 
 GITHUB_CLIENT_ID = '7d4f47c88e6da7febbbb'
 GITHUB_CLIENT_SECRET = 'ecb9f99a072a97dafb9cf7d4ae7974bf6e0a99ea'
